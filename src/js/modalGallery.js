6a779db72787cbabe92a71f1ed6cada426529cf7
vladisluw <komkov222111@gmail.com>
2024-03-15 05:44:07 +0500

    fix 15 03

--- src/js/modalGallery.js
+++ src/js/modalGallery.js
@@ -1,21 +1,32 @@
-export default () => {const openBtn = document.querySelectorAll('[data-attr="modal-gallery__open"]')
+export default () => {
+  const openBtn = document.querySelectorAll('[data-attr="modal-gallery__open"]');
+  const closeBtn = document.querySelector('[data-attr="modal-gallery__close"]');
+  const modalGallery = document.querySelector('#modal-gallery');
+  const modalGalleryContent = modalGallery.querySelector('.gallery');
+  const html = document.querySelector('html');
 
-const closeBtn = document.querySelector('[data-attr="modal-gallery__close"]')
+  if (!openBtn || !closeBtn || !modalGallery) return;
 
-const modalGallery = document.querySelector('#modal-gallery')
+  openBtn.forEach(button => button.addEventListener('click', (e) => {
+      e.preventDefault();
+      modalGallery.classList.add('active');
+      html.style.overflow = 'hidden';
+  }));
 
-const html = document.querySelector('html')
-if (!openBtn || !closeBtn || !modalGallery) return
+  closeBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      modalGallery.classList.remove('active');
+      html.style.overflow = 'auto';
+  });
 
-openBtn.forEach(button => button.addEventListener('click', (e) => {
-  e.preventDefault()
-  modalGallery.classList.add('active')
-  html.style.overflow = 'hidden'
-}))
+  modalGallery.addEventListener('click', (e) => {
+      if (e.target === modalGallery) {  // Проверка, был ли клик на самом модальном окне
+          modalGallery.classList.remove('active');
+          html.style.overflow = 'auto';
+      }
+  });
 
-closeBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  modalGallery.classList.remove('active')
-  html.style.overflow = 'auto'
-
-})}
+  modalGalleryContent.addEventListener('click', (e) => {
+      e.stopPropagation();
+  });
+}
